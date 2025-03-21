dad5aae80dc80fa37d70eb15bfff19d125e3a8f2
jaddu17 <shafajaddu@gmail.com>
2025-03-21 12:51:21 +0700

    Memuat ulang data di DataGridView setelah penghapusan

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -131,7 +131,8 @@ namespace CRUDSederhana
 
                                 if (rowsAffected > 0)
                                 {
-
+                                    MessageBox.Show("Data berhasil dihapus!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    LoadData();
                                 }
                             }
                         }
