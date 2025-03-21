fd96480b4e562df62f3ba2fb3231ef86197faa39
jaddu17 <shafajaddu@gmail.com>
2025-03-21 14:00:57 +0700

    Membuat objek SqlCommand untuk menjalankan query

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -186,6 +186,7 @@ namespace CRUDSederhana
                         }
                         conn.Open();
                         string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
                     }
                 }
             }
