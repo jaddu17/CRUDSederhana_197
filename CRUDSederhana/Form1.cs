69db533fe95ef4248e0d6e593981e0a9a459bac4
jaddu17 <shafajaddu@gmail.com>
2025-03-21 12:40:45 +0700

    Memeriksa apakah data berhasil ditambahkan

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -88,6 +88,10 @@ namespace CRUDSederhana
                         cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text.Trim());
 
                         int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+
+                        }
                     }
                 }
             }
