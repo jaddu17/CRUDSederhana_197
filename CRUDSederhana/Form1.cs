decd4e6d97958cbb048c8ec165b5075151381bee
jaddu17 <shafajaddu@gmail.com>
2025-03-21 12:13:08 +0700

    menghubungkan ke sql

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -12,6 +12,9 @@ namespace CRUDSederhana
 {
     public partial class Form1 : Form
     {
+        // Connection String untuk menghubungkan ke database SQL Server
+        private string connectionString = "Data Source=DESKTOP-KMRCV2Q\\JADDU;" +
+                                         "Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
