7981fe0a44bb975d924fd76c47f7ba210430be1f
jaddu17 <shafajaddu@gmail.com>
2025-03-21 12:52:56 +0700

    Menampilkan pesan error jika terjadi kesalahan saat menghapus data

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -141,6 +141,10 @@ namespace CRUDSederhana
                                 }
                             }
                         }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
                     }
                 }
             }
