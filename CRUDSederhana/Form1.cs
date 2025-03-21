c0c0f80a1a8cca85b5ae6d45b1567f80e394f8ba
jaddu17 <shafajaddu@gmail.com>
2025-03-21 12:24:19 +0700

    membuat objek SqlDataAdapter untuk mengeksekusi query dan mengambil data

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -49,6 +49,7 @@ namespace CRUDSederhana
                 {
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
                 }
             }
         }
