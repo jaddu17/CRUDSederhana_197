7eed4d79c110587fab62e23650143c0f27cecff5
jaddu17 <shafajaddu@gmail.com>
2025-03-21 14:13:46 +0700

    Mengisi nilai dari baris yang dipilih ke dalam TextBox di form

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -224,7 +224,12 @@ namespace CRUDSederhana
             {
                 DataGridViewRow row = dgvMahasiswa.Rows[e.RowIndex];
 
-               
+                //Coba gunakan indeks jika "NIM" tidak ditemukan
+                txtNIM.Text = row.Cells[0].Value.ToString();
+                txtNama.Text = row.Cells[1].Value?.ToString();
+                txtEmail.Text = row.Cells[2].Value?.ToString();
+                txtTelepon.Text = row.Cells[3].Value?.ToString();
+                txtAlamat.Text = row.Cells[4].Value?.ToString();
             }
         }
     }
