e22c9f09b8d77f71216009a0c3560b9c4221e334
jaddu17 <shafajaddu@gmail.com>
2025-03-21 14:12:53 +0700

    Memastikan bahwa indeks baris yang diklik adalah valid

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -220,7 +220,10 @@ namespace CRUDSederhana
         }
         private void dgvMahasiswa_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            if (e.RowIndex >= 0)
+            {
+                
+            }
         }
     }
 }
