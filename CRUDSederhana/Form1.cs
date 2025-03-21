992d60fb0dc922ca626fd678acbc2a4f565684c5
jaddu17 <shafajaddu@gmail.com>
2025-03-21 13:38:41 +0700

    Menampilkan pesan error jika data tidak berhasil ditambahkan

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -194,7 +194,13 @@ namespace CRUDSederhana
                             LoadData();
                             ClearForm(); // Auto Clear setelah tambah data
                         }
-                        
+                        else
+                        {
+                            MessageBox.Show("Data tidak berhasil ditambahkan!", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+               
             }
         }
     }
