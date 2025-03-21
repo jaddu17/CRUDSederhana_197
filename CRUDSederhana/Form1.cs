a2e6688cde489319e597d82b8bed179b579bf69e
jaddu17 <shafajaddu@gmail.com>
2025-03-21 13:59:45 +0700

    Query SQL untuk menambahkan data ke dalam tabel Mahasiswa

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -185,6 +185,7 @@ namespace CRUDSederhana
                             return; 
                         }
                         conn.Open();
+                        string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
                     }
                 }
             }
