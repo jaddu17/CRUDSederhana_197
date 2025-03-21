97ec9e5ab38eff4a080fd28431c90308e4336128
jaddu17 <shafajaddu@gmail.com>
2025-03-21 12:48:55 +0700

    Membuat objek SqlCommand untuk menjalankan query

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -123,6 +123,11 @@ namespace CRUDSederhana
                             string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].Value.ToString();
                             conn.Open();
                             string query = "DELETE FROM Mahasiswa WHERE NIM = @NIM";
+
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+
+                            }
                         }
                     }
                 }
