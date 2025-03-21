9deb9ef552ae09506b9b2d2f65ad25a95b2a1682
jaddu17 <shafajaddu@gmail.com>
2025-03-21 14:07:05 +0700

    Menampilkan pesan sukses jika data berhasil ditambahkan

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -196,7 +196,7 @@ namespace CRUDSederhana
                         int rowsAffected = cmd.ExecuteNonQuery();
                         if (rowsAffected > 0)
                         {
-
+                            MessageBox.Show("Data berhasil ditambahkan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         }
                     }
                 }
